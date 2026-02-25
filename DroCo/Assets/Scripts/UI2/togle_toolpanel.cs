2f911e8601ce4371a0b3ececacc56fc62107633a
Vaclav Sovak <venca03@centrum.cz>
2026-02-25 10:57:39 +0100

    fixed tool panel animation, ready to implement tool panel functionality

--- DroCo/Assets/Scripts/UI2/togle_toolpanel.cs
+++ DroCo/Assets/Scripts/UI2/togle_toolpanel.cs
@@ -2,27 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class togle_toolpanel : MonoBehaviour
-{
+public class togle_toolpanel : MonoBehaviour {
     public Animator animator;
 
-    private bool isOpen = false;
-
-    public void TogglePanel()
-    {
-        isOpen = !isOpen;
-        animator.SetBool("IsOpen", isOpen);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void TogglePanel() {
+        bool isOpen = animator.GetBool("isOpen");
+        animator.SetBool("isOpen", !isOpen);
     }
 }
